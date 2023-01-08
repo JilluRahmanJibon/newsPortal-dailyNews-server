dd8d5f3394c3b929806b10931ba2fff692ce45a1
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2023-01-08 13:41:27 +0600

    fake data added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
-
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 app.use(cors())
@@ -11,9 +12,74 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.send('newsPortal-dailyNewsn-server is running')
+    res.send('newsPortal-dailyNewn-server is running')
 })
 
 app.listen(port, () => {
     console.log('server is running on ',port);
 })
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.jf2skzr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send('unAuthorized')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "forbidden access" })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
+async function run() {
+    const usersCollections= client.db('newsPortal').collection('users')
+
+
+
+    // verify admin 
+    const verifyAdmin = async (req, res, next) => {
+        const decodedEmail = req.decoded.email
+        const query = { email: decodedEmail }
+        const user = await usersCollections.findOne(query)
+
+        if (user?.role !== 'admin') {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        next()
+    }
+    // verify publisher 
+    const verifyPublisher = async (req, res, next) => {
+        const decodedEmail = req.decoded.email
+        const query = { email: decodedEmail }
+        const user = await usersCollections.findOne(query)
+
+        if (user?.role !== 'publisher') {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        next()
+    }
+
+
+
+
+
+
+}
+run().catch(err => {
+    console.log(err);
+});
